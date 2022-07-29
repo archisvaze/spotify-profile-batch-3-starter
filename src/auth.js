109060f0f396c2e6fa2b1c9931b22a189525441e
archis <vazearchis@gmail.com>
2022-07-29 13:03:13 +0530

    added auth.js

---
+++ src/auth.js
@@ -0,0 +1,5 @@
+const CLIENT_ID = "6e26e74044fa468d81eabcf6fea33e9b"
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+const CLIENT_SECRET = "f764b0ca59bb4ccb91d81d3ede3237c7"
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import { save } from "../slices/mySlice"
 function App(props){
