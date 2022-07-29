fa14f828a6a5c21f637fd0f62f429605d0fff1d9
archis <vazearchis@gmail.com>
2022-07-29 12:30:49 +0530

    boilerplate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css"
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
-);
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
