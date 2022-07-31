9670f1c64d1c623c3cbf599987d57061a8c2b7de
Ibrahim Saffi <ibrahimsaffinew@gmail.com>
2022-08-01 02:11:30 +0500

    commit

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -8,8 +8,10 @@ export default function ProfilePage() {
   let state = useSelector(state => state.myState)
   let user = state.user  
 
+  
   useEffect(() => {
     dispatch(clearFlag());
+
     dispatch(setFlag("profile"));
   }, [])
 
@@ -18,46 +20,56 @@ export default function ProfilePage() {
   return (
     <div className='profile-page-container'>
      <div className="top-profile-container">
-       <img src={user.images[0].url} alt="" />
+       <img className='profile-pic' src={user.images[0].url} alt="" />
        <h1>{user.display_name}</h1>
-       <div className="followers">
-        <div>
-          <h1>{user.followers.total}</h1>
+       <div className="profile-stats">
+        <div className='stats-container'>
+          <h1 className='stat-nums'>{user.followers.total}</h1>
           <p>FOLLOWERS</p>
         </div>
-        <div>
-        <h1>{user.followers.total}</h1>
+        <div  className='stats-container'>
+        <h1 className='stat-nums'>{user.followers.total}</h1>
           <p>FOLLOWING</p>
         </div>
-        <div>
-        <h1>{user.followers.total}</h1>
+        <div  className='stats-container'>
+        <h1 className='stat-nums'>{user.followers.total}</h1>
           <p>PLAYLIST</p>
         </div>
        </div>
-       <button>LOGOUT</button>
+       <button className='profile-btns'>LOGOUT</button>
      </div>
      <div className="bottom-profile-container">
         <div className='profile-top-artists-container' >
            <div className='profile-artist-label' >
             <h3>Top Artists of All time</h3>
-            <button>SEE MORE</button>
+            <button className='profile-btns'>SEE MORE</button>
            </div>
-        </div>
         <div className='profile-artists-list' >
-          <div className='profile-artists-card' >
+           {state.artists.map(ele=>{
+             <div className='profile-artists-card' >
             <img src="" alt="" />
             <p></p>
           </div>
+           })}
+           <div className='profile-artists-card' >
+            <img className='artist-pic' src={user.images[0].url} alt="" />
+            <p>Artist name</p>
+          </div>
         </div>
+            </div>
         <div className='profile-top-tracks-container' >
         <div className='profile-tracks-label' >
             <h3>Top Tracks of All time</h3>
-            <button>SEE MORE</button>
+            <button className='profile-btns'>SEE MORE</button>
            </div>
            <div className='profile-tracks-list' >
           <div className='profile-tracks-card' >
-            <img src="" alt="" />
-            <p></p>
+            <img className='album-pic' src={user.images[0].url} alt="" />
+            <div className='track=-detail' >
+            <p>Track Name</p>
+            <p>Artists of track</p>
+            </div>
+            <p className='track-length' >3:00</p>
           </div>
         </div>
         </div>
