a0dd641e8c2a0bdf23b796ab45dbb0ab536476ca
Archis <92965519+archisvaze@users.noreply.github.com>
2022-09-12 19:26:46 +0530

    Delete auth.js

--- src/auth.js
+++
@@ -1,5 +0,0 @@
-const CLIENT_ID = "88b1fff7f3af426ba3472d3dcc2fa959"
-const REDIRECT_URI = "http://localhost:3000"
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-const RESPONSE_TYPE = "token"
-const CLIENT_SECRET = "f764b0ca59bb4ccb91d81d3ede3237c7"
